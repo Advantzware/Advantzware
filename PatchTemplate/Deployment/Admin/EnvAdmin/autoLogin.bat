a9bac81cf3c1fea51d53ed127e2678d58bccc527
Mark Tyndall <mark.tyndall@advantzware.com>
2018-06-22 08:19:54 -0400

    27105 - auto-login minor adjustments

--- PatchTemplate/Deployment/Admin/EnvAdmin/autoLogin.bat
+++ PatchTemplate/Deployment/Admin/EnvAdmin/autoLogin.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
 REM This a template for a batch script that allows automated log in into advantzware envronments
 
-
 REM Check advantzware.ini for variable defaults
 CALL ..\getSysVars.bat
 REM Do this AFTER you get the system variables
@@ -10,7 +9,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 SET DLC=%DLCDir%
 !Drive!
 CD \!topDir!\!adminDir!\!envAdmin!
-CALL !DLC!\bin\prowin -basekey INI -ininame dbms.ini -pf advantzware.pf -p asiLogin.w -param "username,password,16.7.12,Advantzware,asiTest167"
+START !DLC!\bin\prowin -basekey INI -ininame dbms.ini -pf advantzware.pf -p asiLogin.w -param "asi,Package99,16.7.12,Advantzware,asiTest167"
 GOTO :EXIT
 
 :EXIT
---
+++ PatchTemplate/DirectoryStructure/Admin/EnvAdmin/autoLogin.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+REM This a template for a batch script that allows automated log in into advantzware envronments
+
+REM Check advantzware.ini for variable defaults
+CALL ..\getSysVars.bat
+REM Do this AFTER you get the system variables
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+SET DLC=%DLCDir%
+!Drive!
+CD \!topDir!\!adminDir!\!envAdmin!
+START !DLC!\bin\prowin -basekey INI -ininame dbms.ini -pf advantzware.pf -p asiLogin.w -param "asi,Package99,16.7.12,Advantzware,asiTest167"
+GOTO :EXIT
+
+:EXIT
