6bbf40e6933a4e7c36c055db43241940582cbd4b
Mark Tyndall <mark.tyndall@advantzware.com>
2019-11-02 15:19:52 -0400

    pre-patch directory cleanup

--- Deployment/SchemaChanges/stdMenu/ApiInbound/triggers/audittbl.d
+++
@@ -1,6 +0,0 @@
-"APIInbound" yes yes yes no
-"APIInboundDetail" yes yes yes no
-"APIInboundEvent" yes yes yes no
-"APIOutbound" yes yes yes no
-"APIOutboundDetail" yes yes yes no
-"APIOutboundEvent" yes yes yes no
