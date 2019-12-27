949047f788f4b8ff69fa56d26f350ea9252136cf
Mark Tyndall <mark.tyndall@advantzware.com>
2019-12-27 16:15:11 -0500

    Files required for PATCH16.15.00

---
+++ Deployment/Patch/DataFiles/APIData/resource.d
@@ -0,0 +1,21 @@
+"201909112818103814740" "" "Devel" "Node" "API Node Name" "8443" yes ? "" "api/startNodeListener.bat" "api/stopNodeListener.bat" "Running" "" 2019-12-27T12:44:54.364 no 2 60
+"201909112818103814741" "" "Devel" "AdminServer" "AWS Server" "20932" yes ? "" "api/startAdminServer.bat" "api/stopAdminServer.bat" "Running" "" 2019-12-27T12:44:55.853 no 3 60
+"201909112818103814742" "" "Devel" "NameServer" "NS1" "5162" yes ? "" "api/startNameServer.bat" "api/stopNameServer.bat" "Running" "" 2019-12-27T12:44:55.136 no 4 60
+"201909112818103814743" "" "Devel" "AppServer" "Advantzware_API" "3093" yes ? "" "api/startAppServer.bat" "api/stopAppServer.bat" "Running" "" 2019-12-27T12:44:54.760 no 5 60
+"201912131265604519042" "001" "Devel" "ASI" "cXML" "" yes 2019-12-13T03:36:13.756 "user1" "api\StartASIMonitor.bat" "" "Stopped" "Last update time: 12/27/2019 12:28:24.407" 2019-12-27T12:28:24.407 no 8 60
+"201912131266904519043" "001" "Devel" "ASI" "fgXML" "" yes 2019-12-13T03:36:13.787 "user1" "api\StartASIMonitor.bat" "" "Stopped" "Last update time: 12/26/2019 08:22:19.925" 2019-12-26T08:22:19.925 no 9 60
+"201912131268204519044" "001" "Devel" "ASI" "RelXML" "" yes 2019-12-13T03:36:44.022 "user1" "api\StartASIMonitor.bat" "" "Stopped" "Last update time: 12/26/2019 08:22:20.554" 2019-12-26T08:22:20.554 no 10 60
+"201912131273004519045" "001" "Devel" "ASI" "RFID" "" yes 2019-12-13T03:36:44.042 "user1" "api\StartASIMonitor.bat" "" "Stopped" "Last update time: 12/26/2019 08:22:21.758" 2019-12-26T08:22:21.758 no 11 60
+"201912131274504519046" "001" "Devel" "ASI" "jobXML" "" yes 2019-12-13T03:36:13.814 "user1" "api\StartASIMonitor.bat" "" "Stopped" "Last update time: 12/26/2019 08:22:28.035" 2019-12-26T08:22:28.035 no 12 60
+.
+PSC
+filename=serverResource
+records=0000000000009
+ldbname=ASI
+timestamp=2019/12/27-15:01:30
+numformat=44,46
+dateformat=mdy-1950
+map=NO-MAP
+cpstream=IBM850
+.
+0000001737
---
+++ Deployment/Patch/DataFiles/estCostCategory.d
@@ -0,0 +1,53 @@
+"201907234774903600441" "boardMinDiff" "costGroup1" "Board Cost - Min Difference" "Board Cost - Min Difference" yes yes no yes no "" no
+"201907234774903600438" "boardNoWaste" "costGroup1" "Board Cost - No Waste" "Board Cost - No Waste" yes yes no yes no "" no
+"201907234774903600440" "boardRunWaste" "costGroup1" "Board Cost - Run Waste" "Board Cost - Run Waste" yes yes no yes no "" no
+"201907234774903600442" "boardSetupVend" "costGroup1" "Board Cost - Vendor Setup" "Board Cost - Vendor Setup" yes yes no yes no "" no
+"201907234774903600439" "boardSetupWaste" "costGroup1" "Board Cost - Setup Waste" "Board Cost - Setup Waste" yes yes no yes no "" no
+"201907234774903600482" "commission" "costGroup20" "Commission" "Commission" no no no no yes "001" no
+"201907234774903600468" "CostCategory67" "costGroup4" "Material Handling Charge" "Material Handling Charge" no no no no yes "" no
+"201907234774903600469" "CostCategory68" "costGroup4" "Finished Goods Handling Charge" "Finished Goods Handling Charge" no no no no yes "" no
+"201907234774903600446" "matMinDiff" "costGroup1" "Material Cost - Min Difference" "Material Cost - Min Difference" no yes no yes no "" no
+"201907234774903600443" "matNoWaste" "costGroup1" "Material Cost - No Waste" "Material Cost - No Waste" no yes no yes no "" no
+"201907234774903600445" "matRunWaste" "costGroup1" "Material Cost - Run Waste" "Material Cost - Run Waste" no yes no yes no "" no
+"201907234774903600447" "matSetupVend" "costGroup1" "Material Cost - Vendor Setup" "Material Cost - Vendor Setup" no yes no yes no "" no
+"201907234774903600444" "matSetupWaste" "costGroup1" "Material Cost - Setup Waste" "Material Cost - Setup Waste" no yes no yes no "" no
+"201907234774903600458" "mLabCost" "costGroup6" "Misc Labor - Cost - COGS" "Misc Labor - Cost - COGS" no no yes yes no "" no
+"201907234774903600459" "mLabCostSep" "costGroup27" "Misc Labor - Cost - Separate Bill" "Misc Labor - Cost - Separate Bill" no no no no no "" no
+"201907234774903600461" "mLabPrice" "costGroup24" "Misc Labor - Profit - Price" "Misc Labor - Profit - Price" no no no no no "" no
+"201907234774903600460" "mLabProfit" "costGroup6" "Misc Labor - Profit - COGS" "Misc Labor - Profit - COGS" no no yes yes no "" no
+"201907234774903600462" "mLabProfitSep" "costGroup27" "Misc Labor - Profit - Separate Bill" "Misc Labor - Profit - Separate Bill" no no no no no "" no
+"201907234774903600463" "mMatCost" "costGroup3" "Misc Material - Cost - COGS" "Misc Material - Cost - COGS" no yes no yes no "" no
+"201907234774903600464" "mMatCostSep" "costGroup29" "Misc Material - Cost - Separate Bill" "Misc Material - Cost - Separate Bill" no no no no no "" no
+"201907234774903600466" "mMatPrice" "costGroup22" "Misc Material - Profit - Price" "Misc Material - Profit - Price" no no no no no "" no
+"201907234774903600465" "mMatProfit" "costGroup3" "Misc Material - Profit - COGS" "Misc Material - Profit - COGS" no yes no yes no "" no
+"201907234774903600467" "mMatProfitSep" "costGroup29" "Misc Material - Profit - Separate Bill" "Misc Material - Profit - Separate Bill" no no no no no "" no
+"201907234775003600480" "nfFolding" "costGroup33" "Folding" "Folding" no no no no yes "" no
+"201907234774903600475" "nfFreight" "costGroup19" "Freight Charge" "Freight Charge" no no no no yes "" no
+"201907234774903600476" "nfFreightMinDiff" "costGroup19" "Freight Charge - Minimum Difference" "Freight Charge - Minimum Difference" no no no no yes "" no
+"201907234774903600478" "nfGSABoard" "costGroup31" "GS&A Board" "GS&A Board" no no no no yes "" no
+"201907234774903600474" "nfGSALab" "costGroup18" "GS&A Labor" "GS&A Labor" no no no no yes "" no
+"201907234774903600473" "nfGSAMat" "costGroup17" "GS&A Material" "GS&A Material" no no no no yes "" no
+"201907234774903600477" "nfMatMarkup" "costGroup30" "Direct Material Markup" "Direct Material Markup" no no no no yes "" no
+"201907234774903600470" "nfUserDef1" "costGroup13" "User Defined Expense 1" "User Defined Expense 1" no no no no yes "" no
+"201907234774903600471" "nfUserDef2" "costGroup14" "User Defined Expense 2" "User Defined Expense 2" no no no no yes "" no
+"201907234774903600472" "nfUserDef3" "costGroup15" "User Defined Expense 3" "User Defined Expense 3" no no no no yes "" no
+"201907234775003600479" "nfWarehouse" "costGroup32" "Warehousing" "Warehousing" no no no no yes "" no
+"201907234774903600433" "opRunDL" "costGroup4" "Operation Run - DL" "Operation Run - DL" no no yes yes no "" no
+"201907234774903600435" "opRunFO" "costGroup8" "Operation Run - FOH" "Operation Run - FOH" no no yes yes no "" no
+"201907234774903600437" "opRunMinDiff" "costGroup4" "Operation Run - Min Difference" "Operation Run - Min Difference" no no yes yes no "" no
+"201907234774903600434" "opRunVO" "costGroup7" "Operation Run - VOH" "Operation Run - VOH" no no yes yes no "" no
+"201907234774903600430" "opSetupDL" "costGroup4" "Operation Setup - DL" "Operation Setup - DL" no no yes yes no "" no
+"201907234774903600432" "opSetupFO" "costGroup8" "Operation Setup - FOH" "Operation Setup - FOH" no no yes yes no "" no
+"201907234774903600436" "opSetupMinDiff" "costGroup4" "Operation Setup - Min Difference" "Operation Setup - Min Difference" no no yes yes no "" no
+"201907234774903600431" "opSetupVO" "costGroup7" "Operation Setup - VOH" "Operation Setup - VOH" no no yes yes no "" no
+"201907234774903600448" "pLabCost" "costGroup5" "Tooling Labor - Cost - COGS" "Tooling Labor - Cost - COGS" no no yes yes no "" no
+"201907234774903600449" "pLabCostSep" "costGroup26" "Tooling Labor - Cost - Separate Bill" "Tooling Labor - Cost - Separate Bill" no no no no no "" no
+"201907234774903600451" "pLabPrice" "costGroup23" "Tooling Labor - Profit - Price" "Tooling Labor - Profit - Price" no no no no no "" yes
+"201907234774903600450" "pLabProfit" "costGroup5" "Tooling Labor - Profit - COGS" "Tooling Labor - Profit - COGS" no no yes yes no "" no
+"201907234774903600452" "pLabProfitSep" "costGroup26" "Tooling Labor - Profit - Separate Bill" "Tooling Labor - Profit - Separate Bill" no no no no no "" no
+"201907234774903600453" "pMatCost" "costGroup2" "Tooling Material - Cost - COGS" "Tooling Material - Cost - COGS" no yes no yes no "" no
+"201907234774903600454" "pMatCostSep" "costGroup28" "Tooling Material - Cost - Separate Bill" "Tooling Material - Cost - Separate Bill" no no no no no "" no
+"201907234774903600456" "pMatPrice" "costGroup21" "Tooling Material - Profit - Price" "Tooling Material - Profit - Price" no no no no no "" no
+"201907234774903600455" "pMatProfit" "costGroup2" "Tooling Material - Profit - COGS" "Tooling Material - Profit - COGS" no yes no yes no "" no
+"201907234774903600457" "pMatProfitSep" "costGroup28" "Tooling Material - Profit - Separate Bill" "Tooling Material - Profit - Separate Bill" no no no no no "" no
+"201907234774903600481" "pProfit" "costGroup25" "Profit" "Profit" no no no no no "" yes
