7217de7928623b42ce01b007d825cc441a72bf5f
Mark Tyndall <mark.tyndall@advantzware.com>
2022-11-30 15:58:03 -0500

    Advantzware_22.04.05

--- Deployment/Patch/DataFiles/APIData/APIInboundDetail.d
+++ Deployment/Patch/DataFiles/APIData/APIInboundDetail.d
@@ -1,4 +1,4 @@
-"/api/getpriceinuom" "202007072671905413040" "/api/getpriceinuom" "price" "data!1252!31688_47.blb" "Response" "001" 17 1
-"/api/getprice" "202007072894505413042" "/api/getprice" "price" "data!1252!31688_48.blb" "Response" "001" 18 2
-"/api/getpricematrix" "202007074878005413044" "/api/getpricematrix" "price" "data!1252!31688_49.blb" "Response" "001" 19 3
-"/api/getpricematrix" "202007075075005413045" "price" "matrix" "data!1252!31688_50.blb" "Response" "001" 19 4
+"/api/getpriceinuom" "202007072671905413040" "/api/getpriceinuom" "price" "data!1252!60764_47.blb" "Response" "001" 17 1
+"/api/getprice" "202007072894505413042" "/api/getprice" "price" "data!1252!60764_48.blb" "Response" "001" 18 2
+"/api/getpricematrix" "202007074878005413044" "/api/getpricematrix" "price" "data!1252!60764_49.blb" "Response" "001" 19 3
+"/api/getpricematrix" "202007075075005413045" "price" "matrix" "data!1252!60764_50.blb" "Response" "001" 19 4
